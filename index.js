c6cb7c79f38c07a6215d04aaa01e67f623684c9e
uikrati <87569719+uikrati@users.noreply.github.com>
2023-08-16 23:34:47 +0530

    final commit

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-const express = require('express');
-const multer = require('multer');
-const app = express();
-const upload = multer();
-const port = process.env.PORT || 3000;
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config()
+var multer=require('multer');
+var app = express();
 
-// Set up the view engine
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/views/index.html`);
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+const upload=multer({dest:"./public/data/storage"});
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
-
-// Handle the file upload
-app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
-  if (!req.file) {
-    return res.json({ error: 'No file uploaded' });
-  }
-
-  const { originalname, mimetype, size } = req.file;
+app.post("/api/fileanalyse",upload.single('upfile'),(req,res)=>{
+  const{originalname,mimetype,size}=req.file;
   res.json({
-    name: originalname,
-    type: mimetype,
-    size: size
+    name:originalname,
+    type:mimetype,
+    size:size
   });
-});
+})
 
-// Start your server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
 });
